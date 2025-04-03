738212bc46d0c806a5a7a12d21b6dcfe02710d25
Berihuete Nunez, Engel <mastering23@gmail.com>
2025-04-03 12:12:25 -0400

    adding xargs | kwargs

--- functionApp.py
+++ functionApp.py
@@ -1,5 +1,28 @@
-def testing ():
-  print('testing')
+def greeting(name):
+  print(f"Welcome to our App {name}")
+greeting('Mastering23')
+#---------------------#
+#        xargs
+#---------------------#
+print("----xargs--------")
+def mat(*numbs):
+  results = 0
+  for numb in numbs:
+      results += numb
+  print(results)
 
+mat(2, 3, 4, 5)
+mat( 4,5)
+mat( 2.3,-2,10,2.1)
+#---------------------#
+#       kwargs
+#---------------------#
+print("----kwargs--------")
+def get_proudct(**data):
+  print(data["id"],data["name"])
 
-testing()
+get_proudct(
+  id ="23",
+  name="Iphone",
+  desc="New Iphone 16 black case"
+)
